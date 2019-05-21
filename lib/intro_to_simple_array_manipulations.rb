095332225f998ee3c029df84c0c55e10c6fa4b2f
Kevin <kevinlai76@gmail.com>
2019-05-21 22:52:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,23 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end 
+
+def using_pop(array)
+  array.pop
+end 
+
+def pop_with_args(array)
+  return array.pop(2)
+end 
+
+def using_shift(array)
+  array.shift
+end 
+
+def shift_with_args(array)
+  return array.shift(2)
+end 
+
+def using_concat(arr1, arr2)
+  
+end
